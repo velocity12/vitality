88613b7551865f8a09313321d4b76df8a0fe8b11
Peter Werner <pedro@bluehillscycling.com>
2015-02-21 12:37:56 -0500

    Readded Fall Back Image

--- assets/js/plugins/jquery.fs.wallpaper.js
+++ assets/js/plugins/jquery.fs.wallpaper.js
@@ -437,7 +437,7 @@
 			$video.one("loadedmetadata.wallpaper", function(e) {
 				//--This is the issue
 
-				alert("hi")
+				
 				$videoContainer.on(transitionEvent, function(e) {
 					_killEvent(e);
 				
